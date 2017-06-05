6192b82bbbcf185e235d180cf2360d5acee73b13
unknown <jeraldfelller@gmail.com>
2017-06-05 14:22:53 +0800

    First commit

---
+++ Includes/footer.php
@@ -0,0 +1,16 @@
+<!-- Footer -->
+<footer class="py-2 bg-inverse">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; remoteraiser.com 2017</p>
+    </div>
+    <!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="../assets/vendor/jquery/jquery.min.js"></script>
+<script src="../assets/vendor/tether/tether.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+include 'Includes/header.php';
+
+?>
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+        <div class="col-sm-8">
+            <h2 class="mt-4">What We Do</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
+            <p>
+                <a class="btn btn-primary btn-lg" href="#">Call to Action &raquo;</a>
+            </p>
+        </div>
+        <div class="col-sm-4">
+            <h2 class="mt-4">Contact Us</h2>
+            <address>
+                <strong>Start Bootstrap</strong>
+                <br>3481 Melrose Place
+                <br>Beverly Hills, CA 90210
+                <br>
+            </address>
+            <address>
+                <abbr title="Phone">P:</abbr> (123) 456-7890
+                <br>
+                <abbr title="Email">E:</abbr> <a href="mailto:#">name@example.com</a>
+            </address>
+        </div>
+    </div>
+    <!-- /.row -->
+
+    <div class="row">
+        <div class="col-sm-4 my-4">
+            <div class="card">
+                <img class="card-img-top img-fluid" src="assets/images/left-graphic.png" alt="">
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">Let's make a difference together today. We will grow the financial resources for your cause. You will help create jobs for those who need a "leg up" in life, by processing the materials you collect. All while making our planet a better place to live.</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 my-4">
+            <div class="card">
+                <img class="card-img-top img-fluid" src="assets/images/center-graphic.png" alt="">
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">20k + Reused</p>
+                    <p class="card-text">50k + Recycled</p>
+                    <p class="card-text">20k + Reduced</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 my-4">
+            <div class="card">
+                <img class="card-img-top img-fluid" src="assets/images/right-graphic.png" alt="">
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container -->
+<?php
+include 'Includes/footer.php';
+?>
